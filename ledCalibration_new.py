b9035214df5514a05a0f73f5d1cc38818e3e3f5b
ziye xu <dec.ziye@gmail.com>
2015-09-28 11:08:31 -0400

    Change LED Calibration file

--- ledCalibration_new.py
+++ ledCalibration_new.py
@@ -3,74 +3,62 @@ import u6
 import pylab as pl
 from pylab import plot, show
 import numpy as np
-import threading
-from threading import Thread
-import time
-import os
-import sys
-#sys.path.append("/usr/share/adafruit/webide/repositories/Adafruit-Raspberry-Pi-Python-Code/Adafruit_MCP4725")
-#from Adafruit_MCP4725 import MCP4725
-#sys.path.append("/usr/share/adafruit/webide/repositories/Adafruit-Raspberry-Pi-Python-Code/Adafruit_ADS1x15")
-#from Adafruit_ADS1x15 import ADS1x15    
+import time, datetime, os
+
+today = datetime.date.today()
+
+todaystr = today.isoformat()
 
 startV = 0
 stopV = 5
 nVPts = 51
-rangeValue = 28 # max range of power meter (mW)
 
 d = u6.U6() # open LabJack U6
 
-d.getCalibrationData()
+d.getCalibrationData() # get calibration info for U6
 
-# Configuration MCP4725 output channel
-#dac = MCP4725(0x62)
-#voltageSource = 5.0
-#maxVal = 4096
-
-# Configure the ADS1115 to receive parameters from AO
-#ADS1115 = 0x01
-#adc = ADS1x15(ic=ADS1115)
-
-
+#power_2V = 0.64
 # setup plotting
 #pl.ion()
 #pH, = pl.plot(0,0, 'b.-')
 #pl.xlabel('Command Voltage (V)')
 #pl.ylabel('Calculated LED Output (mW)')
 
-# loop and collect data
+# Set DAC0 to 2V and get analog reading from AIN0 to calculate scale
 DAC0_value = d.voltageToDACBits(2, dacNumber = 0, is16Bits = False)
 d.getFeedback(u6.DAC0_8(DAC0_value))
-scale = 0.64/d.getAIN(0) 
+
+power_2V=raw_input('Press enter Power Reading on Photometer: ')
+
+scale = power_2V/d.getAIN(0) 
 
 vLevels = np.linspace(startV, stopV, nVPts)
-respV = vLevels*np.nan
+respV = vLevels*np.nan # initialize led power array
+
 for (tVNum,tV) in enumerate(vLevels):
     
     # set ao value
     volts = (tV*np.ones(1))
     print(volts)
-    #voltsInBits = [int((volts/voltageSource)*maxVal)]
+    
+    # loop through each voltage value and set DAC0
     for val in volts:
-        #dac.setVoltage(val) # np.ones(2) to np.ones(100)
         DAC0_value = d.voltageToDACBits(val, dacNumber = 0, is16Bits = False)
         d.getFeedback(u6.DAC0_8(DAC0_value))
-        time.sleep(2)
+        time.sleep(3)  # wait for 3s
     
-    # read resulting analog
-    #tVolts = adc.readADCSingleEnded(0,4096, 250)/1000
+    # read resulting analog AIN0
     ainValue = d.getAIN(0)
-    tDataCol = (ainValue*scale)#*rangeValue)/2
-    #tDataCol = tVolts
+    tDataCol = (ainValue*scale) #scale the input voltage to power
     print tDataCol
     
-    responseData = tDataCol
+    #responseData = tDataCol
     #responseData = np.array([x for x in tDataCol])
     #load into a vector - discard pts at start for stability
-    respV[tVNum] = np.mean(responseData)
+    respV[tVNum] = np.mean(tDataCol)
     #respV = responseData
 
-    noNanIx = np.logical_not(np.isnan(respV))
+    #noNanIx = np.logical_not(np.isnan(respV))
 
     #pH.set_xdata(vLevels[noNanIx])
     #pH.set_ydata(np.array(respV[noNanIx]))
@@ -82,15 +70,18 @@ for (tVNum,tV) in enumerate(vLevels):
     #pl.pause(1.0)
 DAC0_value = d.voltageToDACBits(0, dacNumber = 0, is16Bits = False)
 d.getFeedback(u6.DAC0_8(DAC0_value))
-print 'Press enter to write LUT and exit'
-nodata=raw_input()
+
+nodata=raw_input('Press enter to write LUT and exit')
 pl.close
-outName = os.path.expanduser('~/Documents/LED_Table/testing.txt')
+
+dirname = "~/Documents/LED_Table/" + todaystr;
+os.mkdir(dirname)
+os.chdir(dirname)
+#outName = os.path.expanduser('~/Documents/LED_Table/LED.txt')
+outName = "LED.txt"
 print outName
 
 fd = open(outName, 'w')
 for (a,b) in zip(vLevels, respV):
     fd.write('%7.4f %7.6f\n' % (a,b))
 fd.close()
-#os.system("sshpass -p hullglick rsync ~/Desktop/testing.txt hullglick@test-rig-2.dhe.duke.edu:~/Documents/LED_Table" )
-#dac.setVoltage(-1)
