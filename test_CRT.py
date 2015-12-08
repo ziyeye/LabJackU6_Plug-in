45f9164ac1965407972e47f57b1395c9bf49bad1
Ashley <wilson@neuro.duke.edu>
2015-12-08 13:23:39 -0500

    CRT test

--- test_CRT.py
+++ test_CRT.py
@@ -7,14 +7,17 @@ d           = u6.U6() # open LabJack U6
 
 d.getCalibrationData() # get calibration info for U6
 runtime=[]
-for i in range(0,1000):
+for i in range(0,2):
+	d.getFeedback(u6.BitDirWrite(0,1))
+	d.getFeedback(u6.BitDirWrite(1,0))
+
 	t1 = time.time()
 
 	d.getFeedback(u6.BitStateWrite(0,1))
 
-	ain = d.getAIN(0)
+	#ain = d.getAIN(0)
+	d.getFeedback(u6.BitStateRead(1))
 
-	if ain > 1:
-		runtime.append(time.time()-t1)
-	
+	runtime.append(time.time()-t1)
+
 print numpy.mean(runtime)
